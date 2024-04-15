f585fd2220a597d7010278b67e911991838df8c9
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 15:04:49 -0300

    Armazenamento e maior valor encontrado em um vetor.c

--- Armazenamento e maior valor encontrado em um vetor.c
+++ Armazenamento e maior valor encontrado em um vetor.c
@@ -4,10 +4,12 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int v[10], l, m;
+	//Preenchimento do vetor
 	for(l = 0; l < 10; l++){
-		printf("Digite o %i� valor a ser armazenado no vetor: \n", l + 1);
+		printf("Digite o %i° valor a ser armazenado no vetor: \n", l + 1);
 		scanf("%i", &v[l]);
 	}
+	//Observando qual é o maior valor
 	m = v[0];
 	
 	for(l = 0; l < 10; l++){
@@ -15,6 +17,7 @@ int main(){
 			m = v[l];
 		}
 	}
+	//Exibindo o maior valor
 	printf("\nO maior valor encontrado no vetor foi: %i. \n", m);
 	return(0);
 }
