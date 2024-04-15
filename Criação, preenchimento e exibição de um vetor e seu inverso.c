1fc5f22c06132c71093d101ab1e5a68d366462f8
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 14:54:10 -0300

    Criação, preenchimento e exibição de um vetor e seu inverso.c

--- Criação, preenchimento e exibição de um vetor e seu inverso.c
+++ Criação, preenchimento e exibição de um vetor e seu inverso.c
@@ -4,15 +4,18 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int A[8], l;
+	//Preenchimento do vetor A
 	for(l = 0; l < 8; l++){
-		printf("Digite o o %i� valor para ser armazenado no vetor A: \n", l + 1);
+		printf("Digite o o %i° valor para ser armazenado no vetor A: \n", l + 1);
 		scanf("%i", &A[l]);
 	}
+	//Exibição do vetor A
 	printf("\nVetor A\n");
 	for(l = 0; l < 8; l++){
 		printf("%i ", A[l]);
 	}
 	printf("\n");
+	//Exibição de um vetor com os valores invertidos do vetor A
 	printf("\nVetor A invertido\n");
 	for(l = 7; l >= 0; l--){
 		printf("%i ", A[l]);
