c9f0ed0dc48b13c654f62ed942c10197cb675c18
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 15:07:22 -0300

    Preenchimento baseado nos valores de um vetor.c

--- Preenchimento baseado nos valores de um vetor.c
+++ Preenchimento baseado nos valores de um vetor.c
@@ -5,10 +5,12 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float a[5], b[5];
 	int l;
+	//Armazenamento de valores no vetor a
 	for(l = 0; l < 5; l++){
-		printf("Digite o %i� valor a ser armazenado no vetor: \n", l + 1);
+		printf("Digite o %i° valor a ser armazenado no vetor: \n", l + 1);
 		scanf("%f", &a[l]);
 	}
+	//Preenchendo o vetor b aplicando condições nos valores do vetor a
 	for(l = 0; l < 5; l++){
 		if(l == 0){
 			b[l] = a[l];
@@ -20,11 +22,13 @@ int main(){
 			b[l] = (a[l] + 5);
 		}
 	}
+	//Exibindo o vetor a 
 	printf("\nVetor A\n");
 	for(l = 0; l < 5; l++){
 		printf("%.1f ", a[l]);
 	}
 	printf("\n");
+	//Exibindo o vetor b
 	printf("\nVetor B\n");
 	for(l = 0; l < 5; l++){
 		printf("%.1f ", b[l]);
