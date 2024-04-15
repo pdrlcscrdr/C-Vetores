3ad31d98945ab8e4a0e953909826277af41fa79d
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 14:56:12 -0300

    Contagem de caracteres.c

--- Contagem de caracteres.c
+++ Contagem de caracteres.c
@@ -5,11 +5,14 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	char nome[100];
 	int i = 0;
+	//Armazenamento de um nome no vetor
 	printf("Digite seu nome completo: ");
 	fgets(nome, 100, stdin);
+	//Contagem de caracteres
 	while(nome[i] != '\0'){
 		i++;
 	}
-	printf("A quantidade de caracteres inseridos foi: %i\n", i);
+	//Exibição da quantidade de caracteres
+	printf("A quantidade de caracteres inseridos (incluindo o \0) foi: %i\n", i);
 	return(0);
 }
